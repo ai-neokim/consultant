cb8c4077ed0ad6109cba0cca81d44cfc5f8fe3b3
ai-neokim <neokim.api@gmail.com>
2024-04-22 11:41:52 +0900

    Debug Change

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%(m4+u+7qh8-vf=04@jx3)=kz)$62wmnkx3*_%ppn(otje832m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['43.203.65.248','consultantx.link']
 

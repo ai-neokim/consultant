d8547b0528db2f3fd083839f3f5083bf1cd5bd3b
ai-neokim <neokim.api@gmail.com>
2024-04-23 11:45:33 +0900

    Debug change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ import environ
 
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.58.70', 'pybo.kr', 'django.pybo.kr']
+ALLOWED_HOSTS = ['43.203.65.248', 'consultantx.link']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = True

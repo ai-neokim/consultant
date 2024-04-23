6148008ce6dd6ca1b8e0d63a6695287f085c6afd
ai-neokim <neokim.api@gmail.com>
2024-04-23 14:23:57 +0900

    source change

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from pybo.models import Question, Answer
+
+from pybo.models import Question, Answer, Comment
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
-        model = Question  # 사용할 모델
-        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+        model = Question
+        fields = ['subject', 'content']
         labels = {
             'subject': '제목',
             'content': '내용',
@@ -18,4 +19,13 @@ class AnswerForm(forms.ModelForm):
         fields = ['content']
         labels = {
             'content': '답변내용',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '댓글내용',
         }
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -3,17 +3,20 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from pybo.forms import QuestionForm
-from pybo.models import Question
+from ..forms import QuestionForm
+from ..models import Question
 
 
 @login_required(login_url='common:login')
 def question_create(request):
+    """
+    pybo 질문등록
+    """
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
-            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.author = request.user  # 추가한 속성 author 적용
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -25,14 +28,19 @@ def question_create(request):
 
 @login_required(login_url='common:login')
 def question_modify(request, question_id):
+    """
+    pybo 질문수정
+    """
     question = get_object_or_404(Question, pk=question_id)
     if request.user != question.author:
         messages.error(request, '수정권한이 없습니다')
         return redirect('pybo:detail', question_id=question.id)
+
     if request.method == "POST":
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.modify_date = timezone.now()  # 수정일시 저장
             question.save()
             return redirect('pybo:detail', question_id=question.id)
@@ -44,19 +52,12 @@ def question_modify(request, question_id):
 
 @login_required(login_url='common:login')
 def question_delete(request, question_id):
+    """
+    pybo 질문삭제
+    """
     question = get_object_or_404(Question, pk=question_id)
     if request.user != question.author:
         messages.error(request, '삭제권한이 없습니다')
         return redirect('pybo:detail', question_id=question.id)
     question.delete()
     return redirect('pybo:index')
-
-
-@login_required(login_url='common:login')
-def question_vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.user == question.author:
-        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
-    else:
-        question.voter.add(request.user)
-    return redirect('pybo:detail', question_id=question.id)

d88fe7e68ac702d92d8ed577480032f3951095c1
ai-neokim <neokim.api@gmail.com>
2024-04-24 16:37:59 +0900

    source change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,22 @@
+import environ
+
 from .base import *
 
-ALLOWED_HOSTS = ['43.203.65.248','consultantx.link']
+ALLOWED_HOSTS = ['43.203.65.248', 'consultantx.link']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+DEBUG = True
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}

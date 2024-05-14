e65d85dc28d5354dd347101cace644367eaa01f0
ai-neokim <neokim.api@gmail.com>
2024-05-14 17:24:32 +0900

    Update models.py

--- pybo/models.py
+++ pybo/models.py
@@ -18,7 +18,8 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    create_date = models.DateTimeField()
+    #create_date = models.DateTimeField()
+    create_date = models.TextField()
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_answer')
 
